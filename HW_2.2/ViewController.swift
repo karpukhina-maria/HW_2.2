0575a54ae52f2ed67089344b27d5635f928e314c
Maria Karpukhina <myshka_n@mail.ru>
2020-02-27 00:32:21 +0300

    Initial Commit

---
+++ HW_2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_2.2
+//
+//  Created by Mary Jane on 27.02.2020.
+//  Copyright © 2020 Maria Karpukhina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
