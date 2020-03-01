2eb1d602268a688c361a99dd54854874a21a9a74
Maria Karpukhina <myshka_n@mail.ru>
2020-03-01 21:32:52 +0300

    Second commit )) Реализован необходимый функционал, но пока без констрейнов :(

--- HW_2.2/ViewController.swift
+++ HW_2.2/ViewController.swift
@@ -9,12 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var rgbView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//      Sliders
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.value = 0
+        redSlider.minimumTrackTintColor = .red
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.value = 0
+        blueSlider.minimumTrackTintColor = .blue
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.value = 0
+        greenSlider.minimumTrackTintColor = .green
+
+//        rgbView
+        rgbView.backgroundColor = UIColor.init(red: CGFloat(redSlider.value),
+                                               green: CGFloat(greenSlider.value),
+                                               blue: CGFloat(blueSlider.value),
+                                               alpha: CGFloat(1.0))
+        
+        
+        
     }
+    
+    @IBAction func redSliderAction() {
+        redValue.text = String(format:"%.2f", redSlider.value)
+        changeViewBackground()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(format:"%.2f", greenSlider.value)
+        changeViewBackground()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(format:"%.2f", blueSlider.value)
+        changeViewBackground()
+    }
+    
+    func changeViewBackground() {
+        let currentRedColor = redSlider.value
+        print(String(currentRedColor))
+        let currentGreenColor = greenSlider.value
+        print(String(currentGreenColor))
+        let currentBlueColor = blueSlider.value
+        print(String(currentBlueColor))
+        rgbView.backgroundColor = UIColor.init(red: CGFloat(currentRedColor),
+                                               green: CGFloat(currentGreenColor),
+                                               blue: CGFloat(currentBlueColor),
+                                               alpha: CGFloat(1.0))
 
-
+    }
 }
 
